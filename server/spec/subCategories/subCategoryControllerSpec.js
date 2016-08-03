37ad9a9ed9dd2eeee26ada8b16d24bb662f5769e
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-03 19:40:06 +0300

    fixed errors with test file

--- server/spec/subCategories/subCategoryControllerSpec.js
+++ server/spec/subCategories/subCategoryControllerSpec.js
@@ -1,39 +1,23 @@
+var app = require('../../server.js');
+var request = require("supertest")(app);
 var expect = require('chai').expect;
-var mongoose = require('mongoose');
-var server = require('../../server.js');
-var chai = require('chai');
-var chaiHTTP = require('chai-http')
-var SubCategoryController = require('../../subCategories/subCategoryController');
-var SubCategory = require('../../subCategories/subCategoryModel.js');
-var should = chai.should();
 
-chai.use(chaiHTTP);
-
-chai.request('http://localhost:8000');
-
-describe('SubCategory Controller', function () {
-
-  it('should create new subCategory in the database', function (done) {
-    chai.request(server)
-    .post('/api/subCategory')
-    .type('json')
-    .send({
-      poster: 'http://screenrant.com/wp-content/uploads/suicide-squad-movie-2016-poster.jpeg',
-      name: 'Suicide Squad',
-      details: 'Action Movie'
-    })
-    .end(function(err,res){
-      res.should.have.status(200);
-      res.should.be.json;
-      res.body.should.be.a('object');
-      res.body.should.have.property('poster');
-      res.body.should.have.property('name');
-      res.body.should.have.property('details');
-      res.body.should.have.property('_id');
-      res.body.poster.should.equal('http://screenrant.com/wp-content/uploads/suicide-squad-movie-2016-poster.jpeg');
-      res.body.name.should.equal('Suicide Squad');
-      res.body.details.should.equal('Action Movie');
-      done();
-    });
+describe("SubCategory Controller", function () {
+  it("should welome me in plain text", function(done) {
+    request.post("/api/subCategory")
+      .send({
+        poster: 'http://screenrant.com/wp-content/uploads/suicide-squad-movie-2016-poster.jpeg',
+        name: 'Suicide Squad',
+        details: 'Action Movie'
+      })
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .end(function (error, result) {
+        expect(error).to.equal(null);
+        expect(result.res.body.poster).to.equal('http://screenrant.com/wp-content/uploads/suicide-squad-movie-2016-poster.jpeg');
+        expect(result.res.body.name).to.equal('Suicide Squad');
+        expect(result.res.body.details).to.equal('Action Movie');
+        done();
+      });
   });
 });
