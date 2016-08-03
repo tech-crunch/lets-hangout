16698b199c3b3ac7da18007e95e3994481d5abce
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-03 17:48:21 +0300

    added a route for the create cubCategory function

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,10 @@
 var helpers = require('./helpers.js'); // our custom middleware
+var subCategoryController = require('../subCategories/subCategoryController.js');
 
 module.exports = function (app, express) {
+
+	// routes for the subCategories
+	app.post('/api/subCategory', subCategoryController.createNew)
 
   // If a request is sent somewhere other than the routes above,
   // send it through our custom error handler
