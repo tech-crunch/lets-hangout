91e2ff1eb079e24631402acf3949ec6c65dab262
housamaldeen <housam993@gmail.com>
2016-08-03 19:00:09 +0300

    Implement getInfo function

--- server/Dashboard/DashboardController.js
+++ server/Dashboard/DashboardController.js
@@ -1,8 +1,8 @@
 var Dashboard = require('./DashboardModel.js');
 
-
 module.exports = {
   createNew: function (req, res, next) {
+    console.log(req.body)
     var newDashboard = new Dashboard();
     newDashboard.save(function(err, newDashboard){
           if(err){
@@ -11,6 +11,14 @@ module.exports = {
             res.status(201).send(newDashboard);
           };
         });
+  },
+  getInfo: function (req, res, next) {
+    Dashboard.findOne({_id: req.params.id})
+        .exec(function(err, dashboard){
+          if(dashboard){
+            return res.status(200).send(dashboard);
+          }
+        })
   }
 }
 
