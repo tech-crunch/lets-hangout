2ed675b7dd31e3b5e0bbdf7b13b61ec6c72a6f51
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-03 11:39:30 +0300

    implemented the subCategory model

---
+++ server/subCategories/subCategoryModel.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+// This is the SubCategory Table.
+var SubCategorySchema = new Schema({
+	poster : {type : String, required : true},
+	name : {type : String, required : true},
+	details: {type: String, required: true},
+	children: [{ type: Schema.Types.ObjectId, ref: 'SubCategory' }]
+});
+
+// Setting up the SubCategory Model
+var SubCategory = mongoose.model('SubCategory', SubCategorySchema);
+
+module.exports = SubCategory;
