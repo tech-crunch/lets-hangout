211636a2d0f9fdaf5d07a5a431632d33412d38f3
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-04 15:34:38 +0300

    impelemented the getChildren function

--- server/subCategories/subCategoryController.js
+++ server/subCategories/subCategoryController.js
@@ -34,5 +34,18 @@ module.exports = {
         res.status(500).send('No such subCategory exists');
       }
     });
+	},
+
+	// function to get array of children object ids
+	getChildren : function (req, res) {
+		var id = req.params.id.toString();
+		SubCategory.findOne({_id: id})
+    .exec(function(err, subCategory){
+      if(subCategory){
+        res.status(200).send( { children : subCategory.children } );
+      } else{
+        res.status(500).send('No such subCategory exists');
+      }
+    });
 	}
 };
