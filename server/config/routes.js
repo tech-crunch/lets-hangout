7935f520ab06c385c3b29db415ac410f336a4240
sarya-alsayed <e.saryaa@gmail.com>
2016-08-07 19:45:49 +0300

    closes #16

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,6 +3,7 @@ var categoryController = require ('../categories/categoryController.js');
 var subCategoryController = require('../subCategories/subCategoryController.js');
 var DashboardController = require('../Dashboard/DashboardController.js');
 var categoryController = require ('../categories/categoryController.js');
+var groupController = require('../groups/groupController');
 
 module.exports = function (app, express) {
 	
@@ -25,6 +26,12 @@ module.exports = function (app, express) {
 	app.get('/api/dashboard/:id', DashboardController.getInfo, helpers.errorHandler)
 	app.put('/api/dashboard/addOption/:id', DashboardController.addOption, helpers.errorHandler)
 	
+	// routes for the groups
+	app.post('/api/group/user/:id',groupController.createNewGroup, helpers.errorHandler);
+	app.post('/api/group/:groupName',groupController.addFriendsToGroup, helpers.errorHandler);
+	app.delete('/api/group/:groupName',groupController.removeFriendFromGroup, helpers.errorHandler);
+	app.delete('/api/:groupName',groupController.deleteGroup, helpers.errorHandler);
+	app.get('/api/:groupName',groupController.getInfo, helpers.errorHandler);
 	// If a request is sent somewhere other than the routes above,
 	// send it through our custom error handler
 	app.use(helpers.errorLogger);
---
+++ server/spec/groups/groupControllerSpec.js
@@ -0,0 +1,116 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var request = require("supertest");
+var app = require('../../server.js');
+var Group = require('../../groups/groupModel.js');
+var User = require('../../users/userModel.js');
+var mongoose = require('mongoose')
+
+var should = chai.should();
+chai.use(chaiHttp);
+
+describe('Group Controller', function () {
+  beforeEach(function (done) {
+    mongoose.connection.db.dropDatabase(done);
+  });
+
+  it('should create new group in database responds with a 201 (Created)', function (done) {
+    var newUser = new User({username:"usertest",authentication:"test"});
+    newUser.save(function (err,user){
+      chai.request(app)
+      .post('/api/group/user/'+user._id)
+      .send({groupName:"testGroup"})
+      .end(function(err, res){
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        done();
+      });
+
+    })
+  });
+
+  it('should responed with 500 error when trying to create Empty Group ', function (done) {
+    var newUser = new User({username:"usertest",authentication:"test"});
+    newUser.save(function (err,user){
+      chai.request(app)
+      .post('/api/group/user/'+user._id)
+      .send({})
+      .end(function(err, res){
+        res.should.have.status(500);
+        done();
+      });
+    })
+  });
+  
+  it('should add new friend to Group on /api/group/:groupName POST', function (done) {
+    var newGroup = new Group({groupName:"group1"});
+    newGroup.save(function (err,group){
+      var newUser = new User({username:"usertest",authentication:"test"});
+      newUser.save(function (err,user){
+      chai.request(app)
+      .post('/api/group/'+group.groupName)
+      .send({username:"usertest"})
+      .end(function(err, res){
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        done();
+      });
+      })
+    })
+  });
+   
+
+  it('should remove friend from Group on /api/group/:groupName DELETE', function (done) {
+    var newGroup = new Group({groupName:"group1"});
+    newGroup.save(function (err,group){
+      var newUser = new User({username:"usertest",authentication:"test"});
+      newUser.save(function (err,user){
+      chai.request(app)
+      .delete('/api/group/'+group.groupName)
+      .send({username:"usertest"})
+      .end(function(err, res){
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        done();
+      });
+    })
+    })
+  });
+
+  it('should remove Group from database on /api/:group DELETE', function (done) {
+    var newGroup = new Group({groupName:"group1"});
+    newGroup.save(function (err,group){
+    chai.request(app)
+    .delete('/api/'+group.groupName)
+    .send()
+    .end(function(err, res){
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      done();
+    });
+    })
+  });
+
+  it('should return Group information from database on /api/:group GET', function (done) {
+    var newGroup = new Group({groupName:"group1"});
+    newGroup.save(function (err,group){
+    chai.request(app)
+    .get('/api/'+group.groupName)
+    .send()
+    .end(function(err, res){
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      done();
+    });
+    }) 
+  });
+
+});
+  
+
+
