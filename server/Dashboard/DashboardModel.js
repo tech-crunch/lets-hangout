0075a89e71ba488b8c2023707a2dd284c8fe8dce
hussamEddinAlhendi <housam35@gmail.com>
2016-08-08 10:00:38 +0300

    fix syntax error

--- server/Dashboard/DashboardModel.js
+++ server/Dashboard/DashboardModel.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var DashboardSchema = new Schema({
-	groupId: {type: Schema.Types.ObjectId, ref:'Group'}
+	groupId: {type: Schema.Types.ObjectId, ref:'Group'},
   options: {type: [{ subCategoryId:{ type : Schema.Types.ObjectId, ref: 'SubCategory' }, voting: {type: Number, default: 0}  }]},
   chosenOption: { type : Schema.Types.ObjectId, ref: 'SubCategory' },
   date: {type: Date, default: Date.now}
