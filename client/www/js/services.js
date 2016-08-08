4af6ce21d3e82dca3a6aa1a2c6c427cc465c6c9f
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-08 16:58:15 +0300

    (feat) added a function to get all categories

--- client/www/js/services.js
+++ client/www/js/services.js
@@ -1,1 +1,19 @@
 angular.module('lets-hangout.services', [])
+
+.factory('Categories', function($http){
+	var baseUrl = 'http://letsshangout.herokuapp.com';
+
+	var getAll = function(){
+		return $http({
+			method: 'GET',
+			url: baseUrl + '/api/categories'
+		})
+		.then(function(resp){
+			return resp.data;
+		});
+	};
+
+	return {
+		getAll: getAll
+	};
+});
