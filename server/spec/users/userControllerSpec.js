0babf60663fe2e64e2c085f910f822bdca461be0
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-11 15:08:10 +0300

    (feat) added a spec file for the userController

---
+++ server/spec/users/userControllerSpec.js
@@ -0,0 +1,17 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var app = require('../../server.js');
+var mongoose = require('mongoose');
+var User = require('../../users/userModel.js')
+
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('User Controller', function () {
+
+  beforeEach(function (done) {
+    mongoose.connection.db.dropDatabase(done);
+  });
+
+});
