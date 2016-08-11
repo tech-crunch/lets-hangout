67f547f9ba883acf5c991714983cf307664b3582
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-11 16:47:51 +0300

    (fix) client styling errors

--- client/www/js/dashBoardController.js
+++ client/www/js/dashBoardController.js
@@ -1,68 +1,65 @@
 (function () {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('lets-hangout')
-        .controller('DashBoardController', DashBoardController)
+	angular
+		.module('lets-hangout')
+		.controller('DashBoardController', DashBoardController);
 
-    DashBoardController.$inject = ['$scope','DashBoard', '$location', 'SubCategory'];
+	DashBoardController.$inject = ['$scope', 'DashBoard', '$location', 'SubCategory'];
 
-    function DashBoardController($scope, DashBoard, $location, SubCategory) {
-	 
-     $scope.dash = {};
-     $scope.dash.option = [];
-     $scope.dash.subC = [];
-     $scope.dash.voting = [];
+	function DashBoardController($scope, DashBoard, $location, SubCategory) {
 
-    var dashBoardID = "57ab0d66c665971c0daea5a4"
-  	$scope.getDashBoardInfo = function() {
-  		DashBoard.getInfo(dashBoardID)
-  		.then (function(data) {
-        for (var i = 0; i < data.options.length; i++) {
-          console.log(data.options[i])
-          var vote = data.options[i].voting;
-          var optionID = data.options[i].subCategoryId
-          $scope.dash.voting.push(vote)
-          $scope.dash.option.push(optionID)
-          SubCategory.getInfo(data.options[i].subCategoryId)
-  		   .then(function(subCat){
-            $scope.dash.subC.push(subCat)
-          })
-        }
-  		})
-  	}
-    $scope.getDashBoardInfo();
+		$scope.dash = {};
+		$scope.dash.option = [];
+		$scope.dash.subC = [];
+		$scope.dash.voting = [];
 
-   	$scope.createNewDashBoard = function() {
-      	DashBoard.createNew()
-      	.then( function (data){
-        		console.log(data)
-      	})
-      	.catch(function(err){
-      		console.log(err)
-      	})
-    	}
-    	
-    $scope.eleminateOptions = function(subID) {
-      DashBoard.eleminateOptions(dashBoardID, subID )
-      .then( function(data){
-        console.log(data)
-      })
-      .catch( function(err){
-        console.log(err)
-      })
-    }
+		var dashBoardID = '57ab0d66c665971c0daea5a4';
+		$scope.getDashBoardInfo = function() {
+			DashBoard.getInfo(dashBoardID)
+			.then (function(data) {
+				for (var i = 0; i < data.options.length; i++) {
+					var vote = data.options[i].voting;
+					var optionID = data.options[i].subCategoryId;
+					$scope.dash.voting.push(vote);
+					$scope.dash.option.push(optionID);
+					SubCategory.getInfo(data.options[i].subCategoryId)
+					.then(function(subCat) {
+						$scope.dash.subC.push(subCat);
+					});
+				}
+			});
+		};
+		$scope.getDashBoardInfo();
 
-    $scope.voteForOption = function(optionID) {
-      console.log(optionID)
-      DashBoard.voteForOption(dashBoardID , optionID)
-      .then( function(data) {
-        return data 
-        })
-      .catch(function(err) {
-        console.log(err)
-      })
-    }
-  }      
-} 
-());
+		$scope.createNewDashBoard = function() {
+			DashBoard.createNew()
+			.then( function (data) {
+				console.log(data);
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
+		};
+			
+		$scope.eleminateOptions = function(subID) {
+			DashBoard.eleminateOptions(dashBoardID, subID )
+			.then( function(data) {
+				console.log(data);
+			})
+			.catch( function(err) {
+				console.log(err);
+			});
+		};
+
+		$scope.voteForOption = function(optionID) {
+			DashBoard.voteForOption(dashBoardID, optionID)
+			.then( function(data) {
+				return data;
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
+		};
+	}  
+} ());
--- client/www/js/loginController.js
+++ client/www/js/loginController.js
@@ -1,37 +1,36 @@
 (function () {
-  'use strict';
+	'use strict';
 
-  angular
-    .module('lets-hangout')
-    .controller('LoginController', LoginController)
+	angular
+		.module('lets-hangout')
+		.controller('LoginController', LoginController);
+	
+	LoginController.$inject = ['$scope', '$state', 'auth', 'store'];
 
-  LoginController.$inject = ['$scope', '$state', 'auth', 'store'];
+	function LoginController($scope, $state, auth, store) {
+		var vm = this;
 
-  function LoginController($scope, $state, auth, store) {
-    var vm = this;
+		var doLogin = function() {
+			auth.signin({
+				container: 'lock-container',
+				authParams: {
+					scope: 'openid offline_access',
+					device: 'Mobile device'
+				}
+			}, function (profile, token, accessToken, state, refreshToken) {
+				console.log(profile);
+				// Success callback
+				store.set('profile', profile);
+				store.set('token', token);
+				store.set('accessToken', accessToken);
+				store.set('refreshToken', refreshToken);
+				
+				$state.go('home');
+			}, function (error) {
+				console.log(error);
+			});
+		};
 
-    function doLogin() {
-      auth.signin({
-        container: 'lock-container',
-        authParams: {
-          scope: 'openid offline_access',
-          device: 'Mobile device'
-        }
-      }, function (profile, token, accessToken, state, refreshToken) {
-        console.log(profile);
-        // Success callback
-        store.set('profile', profile);
-        store.set('token', token);
-        store.set('accessToken', accessToken);
-        store.set('refreshToken', refreshToken);
-        
-         $state.go("home");
-      }, function (error) {
-        console.log(error);
-      });
-    };
-
-    doLogin();
-  };
-  
+		doLogin();
+	}
 } ());
