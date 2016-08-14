e6e3febc6b92f5bcd704bfbff92628a2527db9a8
hussamEddinAlhendi <housam35@gmail.com>
2016-08-14 17:07:05 +0300

    Add dashController file

---
+++ client/www/js/dashController.js
@@ -0,0 +1,34 @@
+(function () {
+	'use strict';
+
+	angular
+		.module('lets-hangout')
+		.controller('dashController', dashController);
+
+	dashController.$inject = ['$scope', '$state', '$timeout', '$stateParams', 'ionicMaterialInk', 'ionicMaterialMotion', 'Categories', 'SubCategory'];
+
+	function dashController($scope, $state, $timeout, $stateParams, ionicMaterialInk, ionicMaterialMotion, Categories, SubCategory) {
+		// Activate ink for controller
+	    ionicMaterialInk.displayEffect();
+	    ionicMaterialMotion.pushDown({
+        selector: '.push-down'
+	    });
+	    
+		Categories.getAll()
+		.then(function(categories) {
+			console.log(categories)
+			SubCategory.getChildren(categories[0]._id)
+			.then(function (options) {
+				$scope.options = options;
+				console.log(options);
+			});
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+		
+	    
+		
+
+	}
+} ());
