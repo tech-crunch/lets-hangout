935d6e108a5585481a4528e83e6251ed161c0225
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-14 09:30:43 +0300

    (feat) added static for deployment

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -24,5 +24,6 @@ module.exports = function (app, express) {
 	app.use(morgan('dev'));
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
+	app.use(express.static(__dirname + '/../../client/www'))
 };
 
