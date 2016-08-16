2c28fe4eba4e137f9b69a2e920bbf22bbf84457f
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-16 16:07:22 +0300

    (refactor) groupHome template and controller

--- client/www/js/grouphomeController.js
+++ client/www/js/grouphomeController.js
@@ -5,21 +5,31 @@
 		.module('lets-hangout')
 		.controller('grouphomeController', grouphomeController);
 
-	grouphomeController.$inject = ['$scope', '$state', '$timeout', '$location', '$ionicActionSheet', 'Group', '$stateParams', '$ionicPopup', 'Users', 'store'];
+	grouphomeController.$inject = ['$scope', '$state', '$timeout', '$location', '$ionicActionSheet',
+	'Group', '$stateParams', '$ionicPopup', 'Users', 'store'];
 
-	function grouphomeController($scope, $state, $timeout, $location, $ionicActionSheet, Group, $stateParams, $ionicPopup, Users, store) {
+	function grouphomeController($scope, $state, $timeout, $location, $ionicActionSheet,
+	Group, $stateParams, $ionicPopup, Users, store) {
 
 		// dashboards Information in one group
 		$scope.data = [];
 
+		$scope.userIsAdmin = false;
+
+		$scope.groupName = '';
+		
+		var userProfile = store.get('userProfile');
+
 		var init = function () {
 			Group.groupInfo($stateParams.groupID)
 			.then(function (group) {
+				$scope.userIsAdmin = group.groupAdmin === userProfile.userId ? true : false;
+				$scope.groupName = group.groupName;
 				for (var i = 0; i < group.dashboard.length; i++) {
 					Group.dashboardInfo(group.dashboard[i])
-						.then(function (dashboard) {
-							$scope.data.push(dashboard);
-						});
+					.then(function (dashboard) {
+						$scope.data.push(dashboard);
+					});
 				}
 			})
 			.catch(function (err) {
@@ -29,45 +39,50 @@
 
 		init();
 
-	/////
 		$scope.showActionsheet = function() {
-		
+
+			var buttonClicked = function(index) {
+				switch(index){
+					case 0:
+						createDashboard();
+						break;
+					case 1:
+						groupFriends();
+						$scope.deletingFriendPopUp();
+						break;
+					case 2:
+						allFriends();
+						$scope.addingFriendPopUp();
+						break;
+				}
+			};
+
+			var buttons = [
+				{ text: 'New Event'},
+				{ text: 'Show Members' }
+			];
+
+
+			if ($scope.userIsAdmin) {
+				buttons.push({ text: 'Add Friend' });
+			}
+
 			$ionicActionSheet.show({
-				titleText: 'Group Settings',
-				buttons: [
-					{ text: 'Show Members' },
-					{ text: 'Add Friend' },
-				],
-				destructiveText: 'Delete Group',
+				titleText: 'Settings',
+				buttons: buttons,
+				destructiveText: $scope.userIsAdmin ? 'Delete Group' : null,
 				cancelText: 'Cancel',
 
 				cancel: function() {
-					console.log('CANCELLED');
 				},
-				buttonClicked: function(index) {
-					console.log('BUTTON CLICKED', index);
-					if (index === 1) {
 
-						//$location.path('/groups/friends/' + $stateParams.groupID);
-						allFriends();
-						$scope.showAlert();
-					}
-					
-					if (index === 0) {
-						//move to  groupFriends Page to delete
-						// $location.path('/groups/members/' + $stateParams.groupID);
-						groupFriends();
-						$scope.popup1();
+				buttonClicked: buttonClicked,
 
-					}
-
-				},
 				destructiveButtonClicked: function() {
-					// delete group
 					Group.deletingGroup($stateParams.groupID)
-						.then(function (data) {
-							$location.path('/groups');
-						});
+					.then(function (data) {
+						$state.go('app.home');
+					});
 				}
 			});
 		};
@@ -75,7 +90,7 @@
 		// functions for show facebook friends list for adding to group
 		var allFriends = function () {
 			$scope.users = [];
-			Users.getFriends(store.get('userProfile').userId)
+			Users.getFriends(userProfile.userId)
 			.then(function (users) {
 				Group.groupInfo($stateParams.groupID)
 				.then(function (group) {
@@ -94,23 +109,13 @@
 		// add friend 
 		$scope.addFriend = function (user) {
 			Group.addingFriend($stateParams.groupID, user)
-				.then(function (data) {
-					// document.getElementById(user.username).style.visibility='hidden';
-					allFriends();
-				})
-				.catch(function (err) {
-					console.log(err);
-				});
-		};	
-
-		//try popup
-		$scope.showAlert = function() {
-			var alertPopup = $ionicPopup.alert({
-				title: 'Friends',
-				template: '<ion-list> <ion-item class="item item-avatar"  ng-repeat="user in users"> <img  src={{user.picture}}> {{user.name}} <button ng-click="addFriend(user.userId)" >add</button> </ion-item></ion-list>',
-				scope: $scope
+			.then(function (data) {
+				allFriends();
+			})
+			.catch(function (err) {
+				console.log(err);
 			});
-		};
+		};	
 
 		// functions to show members in group and delete member
 		//show friends in group
@@ -120,13 +125,9 @@
 			.then(function (group) {
 				for (var i = 0; i < group.users.length; i++) {
 					Users.getOne(group.users[i])
-						.then(function (user) {
-							$scope.gfriends.push(user.data);
-						});
-				}
-				if ($scope.gfriends.length === 0) {
-					// $scope.showAlert();
-					console.log('no ff');
+					.then(function (user) {
+						$scope.gfriends.push(user.data);
+					});
 				}
 			})
 			.catch(function (err) {
@@ -137,22 +138,29 @@
 		//delete friend from group
 		$scope.deleteFriend = function (user) {
 			Group.deletingFriend($stateParams.groupID, user)
-				.then(function (data) {
-					groupFriends();
-				})
-				.catch(function (err) {
-					console.log(err);
-				});
+			.then(function (data) {
+				groupFriends();
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
 
 		};
 
-		$scope.popup1 = function() {
+		$scope.addingFriendPopUp = function() {
+			var alertPopup = $ionicPopup.alert({
+				title: 'Friends',
+				template: '<ion-list> <ion-item class="item item-avatar"  ng-repeat="user in users"> <img  src={{user.picture}}> {{user.name}} <button ng-click="addFriend(user.userId)" >add</button> </ion-item></ion-list>',
+				scope: $scope
+			});
+		};
+
+		$scope.deletingFriendPopUp = function() {
 			var alertPopup = $ionicPopup.alert({
 				title: 'Group Members',
-				template: '<ion-list><ion-item ng-repeat="user in gfriends"><img  src={{user.picture}}> {{user.name}}<button ng-click="deleteFriend(user.userId)">delete</button></ion-item></ion-list>',
+				template: '<ion-list><ion-item ng-repeat="user in gfriends"><img  src={{user.picture}}> {{user.name}}<button ng-if="userIsAdmin" ng-click="deleteFriend(user.userId)">delete</button></ion-item></ion-list>',
 				scope: $scope
 			});
 		};
 	}
-
 } ());
