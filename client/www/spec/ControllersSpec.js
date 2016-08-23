e8ddf98d0ca3efe1dbb28185d52d7170101cc3e2
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-23 16:09:32 +0300

    closes #147

--- client/www/spec/ControllersSpec.js
+++ client/www/spec/ControllersSpec.js
@@ -25,7 +25,6 @@ describe('Testing Controllers', function() {
 		it('should have a function called sendMessage', function() {
 			expect(typeof scope.sendMessage).toBe('function');
 		});
-
 	});
 
 	describe('Testing LoginController', function() {
@@ -48,7 +47,6 @@ describe('Testing Controllers', function() {
 		it('should have a function	', function() {
 
 		});
-
 	});
 
 	describe('Testing HomeController', function() {
@@ -77,14 +75,7 @@ describe('Testing Controllers', function() {
 
 		it('should have a function called createGroup', function() {
 			expect(typeof scope.createGroup).toBe('function');
-		});
-
-		// it('should have a function called login', function() {
-		// 	expect(typeof login).toBe('function');
-		// });
-
-		
-
+		});		
 	});
 
 	describe('Testing CardsController', function() {
@@ -124,15 +115,6 @@ describe('Testing Controllers', function() {
 		it('should have a userId in the store local Storage', function() {
 			expect(store.get('userProfile').userId).toBeDefined();
 		});
-
-		// it('should return promise when editing profile', function(){
-
-		//   deferred.resolve({data: {name : 'Movies' , poster: "www.asdfadf.com"}})
-		//   scope.initialize();
-		//   scope.$apply();
-		//   expect(Categories.getAll).toHaveBeenCalled();
-		// })
-
 	});
 
 	describe('Testing DashBoardController', function() {
@@ -220,16 +202,6 @@ describe('Testing Controllers', function() {
 			expect(DashBoard.eleminateOptions).toBeDefined();
 		}]));
 
-		// it('should get `DashBoard.getInfo`promises', inject(['DashBoard', function() {
-		// 	deferred.resolve({options:[],voting:{}})
-		// 	spyOn(DashBoard, 'getInfo').and.returnValue(deferred.promise);
-		// 	$httpBackend.expectGET('templates/dashboard.html').respond({status:200});
-		// 	httpBackend.flush();
-		// 	var func = scope.initialize()
-		// 	scope.$apply();
-		// 	expect(func).toBe(0);
-		// }]));
-
 		it('should get `SubCategory.getSubCategories`promises', inject(['SubCategory', function() {
 			spyOn(SubCategory, 'getSubCategories').and.returnValue(deferred.promise);
 			expect(typeof SubCategory.getSubCategories(12345)).toBe('object');
@@ -239,7 +211,58 @@ describe('Testing Controllers', function() {
 			spyOn(SubCategory, 'getSubCategories').and.returnValue(deferred.promise);
 			expect(typeof SubCategory.getSubCategories(1234)).toBe('object');
 		}]));
+	});
 
+	describe('Testing grouphomeController', function() {
+		var scope, ctrl, $window, $q, deferred, store, Group, ionicDatePickerMock, Users;
 
+		beforeEach(inject(function($controller, $rootScope, _$window_, _$q_, _Group_, _store_, _Users_) {
+			scope = $rootScope.$new();
+			store = _store_;
+			store.set('userProfile', {userId: '123'});
+			$q = _$q_;
+			deferred = _$q_.defer();
+			Group = _Group_;
+			Users = _Users_;
+			spyOn(Group, 'addingFriend').and.returnValue(deferred.promise);
+			ionicDatePickerMock = {
+				openDatePicker: jasmine.createSpy('ionicDatePicker spy').and.returnValue(deferred.promise)
+			};
+
+			$controller('grouphomeController', { 
+				$scope: scope, 
+				Group: _Group_,
+				store: _store_,
+				$window: _$window_,
+				$q: _$q_,
+				ionicDatePicker: ionicDatePickerMock,
+				Users: _Users_
+			});
+		}));
+
+		it('should initialize the data in the scope', function() {
+			expect(scope.data).toBeDefined();
+			expect(scope.groupName).toBeDefined();
+		});
+
+		it('should start with userIsAdmin populated', function () {
+			expect(scope.userIsAdmin).toEqual(false);
+		});
+
+		it('should have a function called initialize', function() {
+			expect(scope.addFriend).toBeDefined();
+		});
+
+		it('should have a userId in the store local Storage', function() {
+			expect(store.get('userProfile').userId).toBeDefined();
+		});
+
+		it('should get the Group services', inject(['Group', function( ) {
+			expect(Group.groupInfo).toBeDefined();
+		}]));
+
+		it('should get the Users services', inject(['Users', function( ) {
+			expect(Users.getFriends).toBeDefined();
+		}]));
 	});
 });
